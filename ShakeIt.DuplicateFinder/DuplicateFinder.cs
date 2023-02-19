8cbdac3b8bc6ad00d8612cd3b20c0df5184d7aa4
Martin Renner <martin+github@planet-renner.net>
2023-02-19 15:03:20 +0100

    Tool to identify duplicate GUIDs in SimHub ShakeIt profiles.

---
+++ ShakeIt.DuplicateFinder/DuplicateFinder.cs
@@ -0,0 +1,41 @@
+﻿namespace ShakeIt.DuplicateFinder;
+
+public class DuplicateFinder
+{
+    public Dictionary<Guid, List<string>> GuidToEffect { get; } = new();
+
+    public void Analyze(IList<ShakeItProfile> profiles)
+    {
+        foreach (var profile in profiles)
+        {
+            AnalyzeEffects(profile.EffectsContainers, profile.Name);
+        }
+
+        foreach (var (guid, list) in GuidToEffect)
+        {
+            if (list.Count > 1)
+            {
+                Console.WriteLine($"{list.Count} duplicates for guid {guid}");
+                foreach (var name in list)
+                {
+                    Console.WriteLine($"  {name}");
+                }
+            }
+        }
+    }
+
+    private void AnalyzeEffects(IList<EffectsContainerBase> effectsContainers, string baseName)
+    {
+        foreach (var effectsContainer in effectsContainers)
+        {
+            if (!GuidToEffect.ContainsKey(effectsContainer.ContainerId))
+            {
+                GuidToEffect[effectsContainer.ContainerId] = new List<string>();
+            }
+
+            GuidToEffect[effectsContainer.ContainerId].Add(baseName + " / " + effectsContainer.Description);
+
+            AnalyzeEffects(effectsContainer.EffectsContainers, baseName + " / " + effectsContainer.Description);
+        }
+    }
+}
---
+++ ShakeIt.DuplicateFinder/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json;
+using ShakeIt.DuplicateFinder;
+
+if (args.Length != 1)
+{
+    Console.WriteLine("Please specify a JSON file to check.");
+    Environment.Exit(1);
+}
+
+var file = args[0];
+
+try
+{
+    using var stream = File.OpenRead(file);
+    var shakeItSettings = JsonSerializer.Deserialize<ShakeItSettings>(stream);
+    if (shakeItSettings == null)
+    {
+        Console.WriteLine($"Could not deserialize file {file}");
+        Environment.Exit(1);
+    }
+
+    Console.WriteLine($"Analyzing {shakeItSettings.Profiles.Count} profiles");
+    var finder = new DuplicateFinder();
+    finder.Analyze(shakeItSettings.Profiles);
+}
+catch (Exception e)
+{
+    Console.WriteLine($"Exception while reading file {file}: {e}");
+    Environment.Exit(1);
+}
