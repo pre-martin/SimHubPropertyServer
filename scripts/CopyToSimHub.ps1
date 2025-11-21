4b06e87a2bb72a410536b3f3c666cace4bc32648
Martin Renner <martin+github@planet-renner.net>
2025-11-21 22:24:49 +0100

    Add run configuration with automatich deployment to SimHub.
    This allows SimHub to be started from the IDE without manual steps.

---
+++ scripts/CopyToSimHub.ps1
@@ -0,0 +1,33 @@
+﻿# Copyright (C) 2025 Martin Renner
+# LGPL-3.0-or-later (see file COPYING and COPYING.LESSER)
+
+# Include as "Before launch" task into IDE. For example:
+# - Tool: pwsh
+# - Args: -NoProfile -File "$ProjectFileDir$/scripts/CopyToSimHub.ps1" -Configuration "$ConfigurationName$"
+
+param(
+    [string]$Configuration = "Debug",
+    [string]$TargetDir = "/Games/SimHub"
+)
+
+$ErrorActionPreference = "Stop"
+
+$ScriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+$ProjectDir = (Get-Item $ScriptDir).Parent
+$SourceDll = Join-Path $ProjectDir "PropertyServer.Plugin/bin/$Configuration/PropertyServer.dll"
+
+if (-not (Test-Path $TargetDir)) {
+    Write-Host "Target directory $TargetDir does not exist."
+    Exit 1
+}
+
+try {
+    Copy-Item -Path $SourceDll -Destination $TargetDir -Force
+    Write-Host "Copied $SourceDll to $TargetDir"
+}
+catch {
+    Write-Error $_
+    Exit 1
+}
+
+Exit 0
