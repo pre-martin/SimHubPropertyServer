95e9097d0cb7c5d7ef044b2ec045b5da23f45f83
Martin Renner <martin+github@planet-renner.net>
2026-07-21 19:58:59 +0200

    Improved helper scripts

--- copyApiFromSimHub.bat
+++ copyApiFromSimHub.bat
@@ -1,17 +1,32 @@
 @echo off
+setlocal
 
-set SIMHUB_DIR=\Games\SimHub
+set "SIMHUB_DIR=\Games\SimHub"
+set "SCRIPT_DIR=%~dp0"
+pushd "%SCRIPT_DIR%"
 
-del /q SimHub\*
+if not exist "%SIMHUB_DIR%\SimHub.Plugins.dll" (
+    echo ERROR: SimHub directory not found or incomplete: "%SIMHUB_DIR%"
+    popd
+    exit /b 1
+)
 
-copy "%SIMHUB_DIR%\WoteverLocalization.dll" SimHub
-copy "%SIMHUB_DIR%\SimHub.Plugins.dll"      SimHub
-copy "%SIMHUB_DIR%\GameReaderCommon.dll"    SimHub
-copy "%SIMHUB_DIR%\log4net.dll"             SimHub
-copy "%SIMHUB_DIR%\Newtonsoft.Json.dll"     SimHub
-copy "%SIMHUB_DIR%\MahApps.Metro.dll"                    SimHub
-copy "%SIMHUB_DIR%\MahApps.Metro.SimpleChildWindow.dll"  SimHub
+if not exist "SimHub\" (
+    mkdir "SimHub"
+)
 
-copy "%SIMHUB_DIR%\Jint.dll" SimHub
-copy "%SIMHUB_DIR%\Acornima.dll" SimHub
-copy "%SIMHUB_DIR%\ICSharpCode.AvalonEdit.dll" SimHub
+del /q "SimHub\*"
+
+copy "%SIMHUB_DIR%\WoteverLocalization.dll" "SimHub\"
+copy "%SIMHUB_DIR%\SimHub.Plugins.dll" "SimHub\"
+copy "%SIMHUB_DIR%\GameReaderCommon.dll" "SimHub\"
+copy "%SIMHUB_DIR%\log4net.dll" "SimHub\"
+copy "%SIMHUB_DIR%\Newtonsoft.Json.dll" "SimHub\"
+copy "%SIMHUB_DIR%\MahApps.Metro.dll" "SimHub\"
+copy "%SIMHUB_DIR%\MahApps.Metro.SimpleChildWindow.dll" "SimHub\"
+
+copy "%SIMHUB_DIR%\Jint.dll" "SimHub\"
+copy "%SIMHUB_DIR%\Acornima.dll" "SimHub\"
+copy "%SIMHUB_DIR%\ICSharpCode.AvalonEdit.dll" "SimHub\"
+
+popd
--- deploy.bat
+++ deploy.bat
@@ -1,6 +1,9 @@
 @echo off
 setlocal
 
+set SCRIPT_DIR=%~dp0
+pushd "%SCRIPT_DIR%"
+
 rem Script to deploy locally.
 rem If SimHub is started with admin privileges, the script has to be started as admin, too.
 
@@ -11,8 +14,17 @@ echo.
 echo Building for configuration: %CONFIG%
 echo.
 
+"C:\Program Files\Microsoft Visual Studio\2022\Community\Msbuild\Current\Bin\amd64\MSBuild.exe" -t:restore -p:Platform="Any CPU" -p:RestorePackagesConfig=true SimHubPropertyServer.sln
+if %errorlevel% neq 0 (
+    popd
+    exit /b 1
+)
+
 "C:\Program Files\Microsoft Visual Studio\2022\Community\Msbuild\Current\Bin\amd64\MSBuild.exe" -p:Configuration=%CONFIG% SimHubPropertyServer.sln
-if %errorlevel% neq 0 exit /b 1
+if %errorlevel% neq 0 (
+    popd
+    exit /b 1
+)
 
 taskkill /im SimHubWPF.exe /t /f
 timeout /t 1
@@ -20,3 +32,5 @@ timeout /t 1
 copy /y PropertyServer.Plugin\bin\%CONFIG%\PropertyServer.dll \Games\SimHub\
 
 start /d \Games\SimHub SimHubWPF.exe
+
+popd
